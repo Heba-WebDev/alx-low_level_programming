35e4bc4e0e45e30dc49fed6f6b08e52c715875a4
Heba Omar <jtvonline@yahoo.com>
2023-03-16 23:17:33 +0300

    a program that prints without using fprint or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-/*
- * main - A program that prints a line without using puts or printf
- * Return: 1
+/**
+* main - A program that prints a line without using printf or puts
+* Return: 0 (success)
 */
 int main(void)
 {
-system("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return 1;
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
