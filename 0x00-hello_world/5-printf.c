c343b4fe5922ab25ae68b63d2f75ef7df4fa6b90
Heba Omar <jtvonline@yahoo.com>
2023-03-16 21:33:44 +0300

    a c program that prints with printf method

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-/*Function Main is the entry of a program*/
+/*
+ * main - A program that prints a line with puts
+ * Return: 0 (success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art, n");
 return (0);
 }
