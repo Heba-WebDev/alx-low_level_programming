1bbbebd178135751e77084ac0d35584814f590f9
Heba-Webdev23 <jtvonline@yahoo.com>
2023-03-16 17:50:24 +0300

    a c progam that prints a text with printf method

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+/*A c program that prints a text with printf method*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
