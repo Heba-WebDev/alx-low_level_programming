2e3a44f952f9ecae0e9866a7ed606cdca507e975
Heba Omar <jtvonline@yahoo.com>
2023-03-17 15:01:12 +0300

    a c program that prints a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,19 @@
 #include<stdlib.h>
 #include<time.h>
-/* more headers go here*/
-/* 
- * main - shows a random number each time the file is executed
- * return: 0 (success)
+/**
+* main - A program that prints a random number
+* Return: 0 (success)
 */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n == 0)
-{
-printf("%d is zero\n", n);
-}
-if(n > 0)
-{
-printf("%d is positive\n", n);
-}
 if(n < 0)
-{
 printf("%d is negative\n", n);
-}
+else if(n > 0)
+printf("%d is positive\n", n);
+else
+printf("%d is zero\n", n);
 return (0);
 }
