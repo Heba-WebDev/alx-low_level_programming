f2f1c4ab2f798da6319c10c249829f8a6a60d6a7
Heba Omar <jtvonline@yahoo.com>
2023-03-17 19:17:51 +0300

    a c program to print all numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+* main - A program that prints all numbers of base 16
+* Return: 0 (success)
+*/
+int main(void)
+{
+int number;
+for (number = 0; number <= 9; number++)
+{
+putchar(number + 48);
+}
+for (number = 0; number < 7; number++)
+{
+putchar(number + 97)
+}
+putchar('\n');
+return (0);
+}
