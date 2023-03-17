44082d91b6dfa27af0249548458284a01a50bf10
Heba Omar <jtvonline@yahoo.com>
2023-03-17 19:27:12 +0300

    a c program to print every possible combination of single digits numbers in base 10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - A program that prints all possible combinations of single-digit numbers
+* Return: 0 (success)
+*/
+int main(void)
+{
+int digit;
+for (digit = 0; digit <= 8; digit++)
+{
+putchar(digit + 48);
+putchar(',');
+putchar(' ');
+}
+putchar('57');
+return (0);
+}
