a9bea382b2749b4c79a6ee6fc0c4696074ac8a76
Heba Omar <jtvonline@yahoo.com>
2023-03-17 18:57:59 +0300

    a c program that prints all single digits numbers in base 10 without using datatype char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - A program that prints all single digits numbers of base 10
+* Return: 0 (success)
+*/
+int main(void)
+{
+int digit;
+for (digit = 0; digit <= 9; digit++)
+{
+printf("%d", digit);
+}
+printf("\n");
+return (0);
+}
