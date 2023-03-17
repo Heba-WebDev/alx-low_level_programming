2226b3100f5700513de9316d68e9b1e2f4a53fd9
Heba Omar <jtvonline@yahoo.com>
2023-03-17 15:15:42 +0300

    a program to print a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include<stdio.h>
 /**
 * main - A program that prints a random number
-* Return: 0 (success)
-*/
+* return: 0 (success)
+**/
 int main(void)
 {
 int n;
@@ -18,7 +18,7 @@ else if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else 
+else
 {
 printf("%d is zero\n", n);
 }
