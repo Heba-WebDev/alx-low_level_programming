b75ec25e967617a4c32b16ba44700897af4142b1
Heba Omar <jtvonline@yahoo.com>
2023-03-22 00:29:04 +0300

    a c program that prints the alphabets ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - A program that prints all alphabets 10 times
+* print_alphabet_x10 - prints all alphabets 10 times
+* Return: Always 0 (Success)
 */
 void print_alphabet_x10(void)
 {
