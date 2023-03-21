6093ecb84dc8d2d69ea46abdac30d7713c066882
Heba Omar <jtvonline@yahoo.com>
2023-03-21 18:27:45 +0300

    a c program that prints all alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - A program that prints all alphabets 
+* Return: 0 (success)
+*/
+int main(void)
+{
+for(int i = 'a'; i <= 'z'; i++) {
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
