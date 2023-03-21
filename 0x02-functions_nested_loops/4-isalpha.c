d44f303e35e427713674b783ab0eab2443b27c91
Heba Omar <jtvonline@yahoo.com>
2023-03-22 02:01:57 +0300

    a c program that prints 1 for a letter and 0 otherwise

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints if a letter is lower cased or not
+ * @c: parameter to be printed
+ * Return: 0 (if c is lowercase) 1 (if c is uppercase)
+ */
+int int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
