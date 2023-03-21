140ef32f4b2edd2f91de1e766d3cb96393d3e115
Heba Omar <jtvonline@yahoo.com>
2023-03-22 00:45:51 +0300

    a c program that prints 0 if a letter is lowercased or 1 if it is not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - prints if a letter is lower cased or not
+ *
+ * Return: 0 (lowecase) 1 (uppercase)
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (0);
+}else
+{
+return (1);
+}
+}
