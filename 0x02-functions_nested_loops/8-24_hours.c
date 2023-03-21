6ec2013216e8d1e78758ff35d82ac35ba25a2025
Heba Omar <jtvonline@yahoo.com>
2023-03-22 02:57:49 +0300

    a c program that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * jack_bauer - prints every minute of the day of Jack Bauer
  * Return: 0 (success)
  */
 void jack_bauer(void)
