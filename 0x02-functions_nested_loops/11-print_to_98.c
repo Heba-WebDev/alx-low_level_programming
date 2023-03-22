1ce53c0e0d61aa2a547960f6fa5e343cf3514335
Heba Omar <jtvonline@yahoo.com>
2023-03-22 04:04:17 +0300

    a c program that prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,6 +8,22 @@
 void print_to_98(int n)
 {
 int i;
+if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+if (i == 98)
+{
+_putchar(i);
+}
+else
+{
+_putchar(i);
+_putchar(' ');
+_putchar(',');
+}
+}
+}
 for(i = n; i <= 98; i++)
 {
 if (i < 98)
