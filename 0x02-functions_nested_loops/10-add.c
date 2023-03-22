d957a6bb8bf982fa5c8d1e9f840fc37e335cd621
Heba Omar <jtvonline@yahoo.com>
2023-03-22 03:55:46 +0300

    a c program that adds two intgers and returns the of their addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - prints the addition result of two given numbers.
+ *@c: parm - first given number
+ *@n: parm - second given number
+ * Return: 0 (success)
+ */
+int add(int n, int c)
+{
+return (n + c);
+}
