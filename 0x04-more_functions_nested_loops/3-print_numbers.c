1ce325766e4ff5b18637fd15d1bfd26c28400b01
Heba Omar <jtvonline@yahoo.com>
2023-03-24 02:03:40 +0300

    a c program that prints digits from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
-* print_numbers - prints numbers from 0 to 9
+*print_numbers - prints digits from 0 to 9
 * 
-* Returns: void
+*Return: void
 */
+
 void print_numbers(void)
 {
-char i;
-for (i = '0'; i <= '9'; i++)
+char c;
+
+for (c = '0'; c <= '9'; c++)
 {
-_putchar(i);
+_putchar(c);
 }
 _putchar('\n');
 }
