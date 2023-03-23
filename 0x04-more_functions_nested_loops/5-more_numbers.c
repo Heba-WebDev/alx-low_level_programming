70e0ed9b1ca0f76c4d548ecbbd1f754e87eb02d4
Heba Omar <jtvonline@yahoo.com>
2023-03-24 02:57:35 +0300

    a c program that prints numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,16 @@ int j;
 
 for (i = 0; i < 10; i++)
 {
-for (j = 0; j <= 14; j++)
+for (j = 0; j < 15; j++)
 {
-_putchar(i + '0');
+if (j >= 10)
+{
+_putchar(j / 10 + '0');
+}
+else
+{
+_putchar(j % 10 + '0');
+}
 }
 _putchar('\n');
 }
