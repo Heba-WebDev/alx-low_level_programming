86c2ccb473b28edd9d0e28b6beb9c1cdcd20b8d3
Heba Omar <jtvonline@yahoo.com>
2023-03-24 02:27:55 +0300

    a c program that prints numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - prints the numbers, from 0 to 14
- * Return: void
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  */
 void more_numbers(void)
 {
