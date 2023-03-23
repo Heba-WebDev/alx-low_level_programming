e3d32b60c6b3dbf7dfdd7c62f5753ed691608e94
Heba Omar <jtvonline@yahoo.com>
2023-03-24 01:54:29 +0300

    a c program that multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digits
+ * @a: first input
+ * @b: second input
+ *
+ * Return: 0 
+ */
+int mul(int a, int b)
+{
+_putchar(a * b);
+_putchar('\n');
+return (0);
+}
