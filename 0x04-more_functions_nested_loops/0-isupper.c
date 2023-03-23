025bf79b2f436d201beb8a3a840388ab6e1bdcd0
Heba Omar <jtvonline@yahoo.com>
2023-03-24 01:43:02 +0300

    a c program that returns 1 for uppercase letters and 0 otherwise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - returns 1 if input is uppercase and 0 otherwise.
+ *
+ * Return: 0 (if upper case) 1 (if not)
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
