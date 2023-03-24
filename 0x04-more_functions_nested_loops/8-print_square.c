f81ee05b489ce7aa77f99d126161bf59fb60f204
Heba Omar <jtvonline@yahoo.com>
2023-03-24 03:31:44 +0300

    a c program that prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_square - prints the square # of a given number
+*@n: input of type int
+*
+*Return: void
+*/
+
+void print_square(int size)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
