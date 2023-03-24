1db871716020269ae48504a2f0ba566a3af7bacc
Heba Omar <jtvonline@yahoo.com>
2023-03-24 03:44:43 +0300

    a c program that prints fizz for numbers that divide by 3 and buzz for numbers that divide by 5 and fizzbuzz if the number divide by both

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+*main - prints fizzbuzz for numbers divided by 3 and 5
+*
+*Return: 0
+*/
+
+int main(void)
+{
+int i;
+for (i = 1; i < 101; i++)
+{
+if (i == 100)
+{
+printf("FizzBuzz");
+}else
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz ");
+}
+else if (i % 3 != 0 && i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+}
+return (0);
+}
