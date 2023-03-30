3145b1b41b851fa8ece00e5faf76da12f06e5f53
Heba Omar <jtvonline@yahoo.com>
2023-03-31 02:38:22 +0300

    a function that appends the src string to the dest string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,11 +8,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int index = 0;
-int dest_len = 0;
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-return (dest);
+	int s = 0, i = 0;
+
+	while (dest[s] != 0)
+	{
+		s++;
+	}
+	while (src[i] != 0)
+	{
+		dest[s + i] = src[i];
+		i++;
+	}
+
+	dest[s + i] = 0;
+	return (dest);
 }
