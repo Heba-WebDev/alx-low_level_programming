71179e2b835c5489ffc6a248326ab3c8b7a4c92f
Heba Omar <jtvonline@yahoo.com>
2023-04-27 20:28:47 +0300

    Header file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: the head of the list to print
+ *
+ * Return: structure of type size_t
+ */
+
+size_t print_list(const list_t *h)
+{
+}
