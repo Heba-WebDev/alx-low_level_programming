45c584c99b70c9e9d3a4839729875931756d1187
Heba Omar <jtvonline@yahoo.com>
2023-04-28 03:32:48 +0300

    Write a function that prints before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,22 +1,15 @@
 #include "lists.h"
 
-/**
- *premain - a function to be executed before the main function
- *Return: void
-*/
-void __attribute__ ((constructor)) premain()
-{
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
-}
+void premain(void) __attribute__ ((constructor));
 
 
 /**
- *main - the main function
- *Return: (0)
+ *premain - executed before the main function
+ *Return: (void)
 */
 
-int main(void)
+void premain(void)
 {
-return (0);
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
