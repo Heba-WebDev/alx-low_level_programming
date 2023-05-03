755369cce2f56a6cbdb6a63e669f187615cdefb3
Heba Omar <jtvonline@yahoo.com>
2023-05-03 03:29:28 +0300

    a function that reverses a listint_t linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -15,13 +15,20 @@ listint_t *reverse_listint(listint_t **head)
 listint_t *next = NULL;
 listint_t *prev = NULL;
 
-while (*head)
+if (!head || !*head)
+{
+return (NULL);
+}
+prev = *head;
+*head = NULL;
+
+while (prev)
 {
 next = (*head)->next;
-(*head)->next = prev;
-prev = *head;
-*head = next;
-}
+(*head)->next = *head;
 *head = prev;
+prev = next;
+}
+
 return (*head);
 }
