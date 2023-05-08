d57b29bc339d22c513e00fbfc97965c85e410ec4
Heba Omar <jtvonline@yahoo.com>
2023-05-09 01:44:09 +0300

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,23 +15,24 @@ int create_file(const char *filename, char *text_content)
 {
 int descriptor;
 int num_written;
+int length;
 
 if (filename == NULL)
-{
 return (-1);
-}
-
-descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 if (text_content != NULL)
 {
-num_written = write(descriptor, text_content, strlen(text_content));
-if (num_written == -1)
+for (length = 0; text_content[length];)
 {
-return (-1);
+length++;
 }
 }
 
+descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+num_written = write(descriptor, text_content, length);
+
+if (num_written == -1 || descriptor == -1)
+return (-1);
 close(descriptor);
 return (1);
 }
