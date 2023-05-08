a3ce94d927035172ec2c729a5f108aa60bbbaeb6
Heba Omar <jtvonline@yahoo.com>
2023-05-09 01:50:16 +0300

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * create_file - creates a file
  * @filename: the name of the file
@@ -10,10 +9,9 @@
  */
 
 
-
 int create_file(const char *filename, char *text_content)
 {
-int descriptor, num_written, length;
+int descriptor, num_written, length = 0;
 
 if (filename == NULL)
 return (-1);
@@ -21,15 +19,13 @@ return (-1);
 if (text_content != NULL)
 {
 for (length = 0; text_content[length];)
-{
 length++;
 }
-}
 
-descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 num_written = write(descriptor, text_content, length);
 
-if (num_written == -1 || descriptor == -1)
+if (descriptor == -1 || num_written == -1)
 return (-1);
 
 close(descriptor);
