2327659d82a6facdc28b604dd1e455d8a3384ed9
Heba Omar <jtvonline@yahoo.com>
2023-05-09 02:22:59 +0300

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,40 +1,41 @@
 #include "main.h"
-
-
+#define BUF_SIZE 1024
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 /**
  * main - program
- * @ac: argument count
+ * @args: argument count
  * @av: argument vector
  *
  * Return: 1 on success 0 on failure
  */
-int main(int ac, char **av)
+
+int main(int args, char **av)
 {
-	int from_fd = 0, to_fd = 0;
-	ssize_t b;
-	char buf[READ_BUF_SIZE];
+int from = 0, to = 0;
+ssize_t b;
+char buf[BUF_SIZE];
 
-	if (ac != 3)
-		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from_fd = open(av[1], O_RDONLY);
-	if (from_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (to_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+if (args != 3)
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+from = open(av[1], O_RDONLY);
+if (from == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+if (to == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
-		if (write(to_fd, buf, b) != b)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
-	if (b == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+while ((b = read(from, buf, BUF_SIZE)) > 0)
+if (write(to, buf, b) != b)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+if (b == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	from_fd = close(from_fd);
-	to_fd = close(to_fd);
-	if (from_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
-	if (to_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+from = close(from);
+to = close(to);
+if (from)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
+if (to)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
 
-	return (EXIT_SUCCESS);
+return (EXIT_SUCCESS);
 }
