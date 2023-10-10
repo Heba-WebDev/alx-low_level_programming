ee46200a5c6627df171ed50e8009e1c77269a86f
Heba Omar <jtvonline@yahoo.com>
2023-10-10 21:16:15 +0300

    Header File

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,9 @@
-#ifndef _SORTING_ALGOS_H_
-#define _SORTING_ALGOS_H_
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
-#include <stdlib.h>
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -16,9 +17,9 @@
  */
 typedef struct listint_s
 {
-	int n;
-	size_t index;
-	struct listint_s *next;
+		int n;
+		size_t index;
+		struct listint_s *next;
 } listint_t;
 
 /**
@@ -34,10 +35,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
